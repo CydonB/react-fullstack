22c14a2a6a8b58c531fd6678a20c7fe2bdcea700
Berk Bilici <berk@bilici.com>
2023-06-25 16:58:15 +0300

    backend integration complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 //https://www.freecodecamp.org/news/how-to-create-a-react-app-with-a-node-backend-the-complete-guide/
 
-import React from "react";
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [gameList, setGameList] = React.useState(null);
 
   React.useEffect(() => {
     fetch("/api")
@@ -14,13 +14,52 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+  React.useEffect(() => {
+    fetch("/api/games")
+      .then((res) => res.json())
+      .then((data) => setGameList(data.games));
+  }, []);
+
+  useEffect(() => {
+    console.log("gameList", gameList);
+  }, [gameList]);
+
   return (
     <div className="App">
+      {!gameList ? (
+        "Loading..."
+      ) : (
+        <>
+          <ul style={{ display: "flex", listStyle: "none" }}>
+            {gameList.map((item) => (
+              <li>
+                <a
+                  href={item.url}
+                  target="blank"
+                  style={{ textDecoration: "none" }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "start",
+                    }}
+                  >
+                    <p>{item.name}</p>
+                    <img
+                      src={item.imageUrl}
+                      style={{ width: "30%", height: "auto" }}
+                    ></img>
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        {!data ? "Loading..." : data}
-        </p>
+        <p>{!data ? "Loading..." : data}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 //https://www.freecodecamp.org/news/how-to-create-a-react-app-with-a-node-backend-the-complete-guide/
 
-
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -9,6 +8,31 @@ const app = express();
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from Berk's Game Server!" });
+});
+
+app.get("/api/games", (req, res) => {
+  res.json({
+    games: [
+      {
+        name: "Rollerdrome",
+        imageUrl:
+          "https://s.yimg.com/os/creatr-uploaded-images/2023-05/725c61d0-fa2e-11ed-9f4f-e05ec454442c",
+        url: "https://store.steampowered.com/agecheck/app/1294420/",
+      },
+      {
+        name: "Stray",
+        imageUrl:
+          "https://s.yimg.com/os/creatr-uploaded-images/2023-05/785b91a0-fa2e-11ed-aff3-7d779f8dd4ed",
+        url: "https://store.steampowered.com/app/1332010/Stray/",
+      },
+      {
+        name: "overwatch2",
+        imageUrl:
+          "https://s.yimg.com/os/creatr-uploaded-images/2023-05/7e91fe10-fa2e-11ed-bef3-f0a2fa3ae316",
+        url: "https://overwatch.blizzard.com/en-us/",
+      },
+    ],
+  });
 });
 
 app.listen(PORT, () => {
